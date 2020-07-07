1b81ad3ad8ebdee8bfdfd7bc9c1093d5ed884453
Aliciap5 <59662250+Aliciap5@users.noreply.github.com>
2020-07-07 18:26:17 -0500

    update

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// Use D3 fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
+d3.json("data/samples.json").then((data) => {
+    var trace1 = {
+        x: data.
+    }
+}
